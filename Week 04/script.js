474d6074e6e485a33a7000a7f00d2d937e5a65c4
Harsha Madushanka <hmharshamadu@gmail.com>
2024-05-01 18:32:15 +0100

    button is pressed display the value in the text box in an alert

--- Week 04/script.js
+++ Week 04/script.js
@@ -1,22 +1,44 @@
-function print5(){
-    alert(1);
-    alert(2);
-    alert(3);
-    alert(4);
-    alert(5);
+// function print5(){
+//     alert(1);
+//     alert(2);
+//     alert(3);
+//     alert(4);
+//     alert(5);
+// }
+
+// function printConsole5(){
+//     console.log(1);
+//     console.log(2);
+//     console.log(3);
+//     console.log(4);
+//     console.log(5);
+// }
+
+
+// print5();
+// print5();
+
+// printConsole5();
+// // printConsole5();
+
+
+// function myLoadFunction() {
+//     var element = document.getElementById('pageheading');
+//     element.firstChild.nodeValue = 'New Heading';
+//     var element = document.getElementById('paragraph');
+//     element.firstChild.nodeValue = 'New paragraph text';
+// }
+
+// document.addEventListener('DOMContentLoaded', myLoadFunction);
+
+function clickFunction() {
+    var element = document.getElementById('myinput');
+    var div = document.getElementById('result');
+    div.firstChild.nodeValue = element.value;
+}
+function myLoadFunction() {
+    var element = document.getElementById('myinput');
+    element.addEventListener('keyup', clickFunction);
 }
 
-function printConsole5(){
-    console.log(1);
-    console.log(2);
-    console.log(3);
-    console.log(4);
-    console.log(5);
-}
-
-
-print5();
-print5();
-
-printConsole5();
-printConsole5();
+document.addEventListener('DOMContentLoaded', myLoadFunction);
