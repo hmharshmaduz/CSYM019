f75e1066ce4222390a6927f75a05903ee7fd8b27
Harsha Madushanka <hmharshamadu@gmail.com>
2024-05-01 18:23:54 +0100

    Print console log

--- Week 04/script.js
+++ Week 04/script.js
@@ -6,5 +6,17 @@ function print5(){
     alert(5);
 }
 
+function printConsole5(){
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
+}
+
+
 print5();
 print5();
+
+printConsole5();
+printConsole5();
